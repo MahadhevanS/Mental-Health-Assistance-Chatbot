1eeb47cbd17f31b96b69af80dfb62fbee768507c
MahadhevanS <mahadhevan0502@gmail.com>
2024-08-25 16:59:21 +0530

    Delete super3_TECH-A-THON directory

--- super3_TECH-A-THON/Super3_TECH-A-THON_2024/chatterbot/logic/__init__.py
+++
@@ -1,9 +0,0 @@
-from chatterbot.logic.logic_adapter import LogicAdapter
-from chatterbot.logic.best_match import BestMatch
-from chatterbot.logic.specific_response import SpecificResponseAdapter
-
-__all__ = (
-    'LogicAdapter',
-    'BestMatch',
-    'SpecificResponseAdapter',
-)
--- super3_TECH-A-THON/Super3_TECH-A-THON_2024/trainer.py
+++
@@ -1,7 +0,0 @@
-def train(bot):
-    from chatterbot.trainers import DictTrainer
-
-    from datalib import data
-
-    trainer=DictTrainer(bot)
-    trainer.train(data)
