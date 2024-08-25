57637fcf7584bd1177aff09799c128e1cf45b383
MahadhevanS <mahadhevan0502@gmail.com>
2024-08-25 16:51:03 +0530

    Add files via upload

---
+++ super3_TECH-A-THON/Super3_TECH-A-THON_2024/chatterbot/logic/__init__.py
@@ -0,0 +1,9 @@
+from chatterbot.logic.logic_adapter import LogicAdapter
+from chatterbot.logic.best_match import BestMatch
+from chatterbot.logic.specific_response import SpecificResponseAdapter
+
+__all__ = (
+    'LogicAdapter',
+    'BestMatch',
+    'SpecificResponseAdapter',
+)
